ea261e01b5aa9ace24db30a1ceaf0fcc0462a470
AttackOnTyler <tylers142007@gmail.com>
2024-02-12 11:32:35 -0500

    added Git commit map

--- lua/plugins/git-config.lua
+++ lua/plugins/git-config.lua
@@ -11,7 +11,8 @@ return {
     {
         'tpope/vim-fugitive',
         config = function()
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git, {})
+            vim.keymap.set("n", "<leader>gc", ":Git commit", {})
         end
     }
 }
